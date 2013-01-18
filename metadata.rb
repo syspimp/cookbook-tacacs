0dac638bc8996ab5efdad805eef459f675b8dfa6
alop <alopgeek@gmail.com>
2013-01-18 15:39:56 -0800

    added apache 2.0

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 maintainer       "AT&T Services, Inc."
 maintainer_email "xc1643@att.com"
-license          "All rights reserved"
+license          "Apache 2.0"
 description      "Installs/Configures tacacs"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
 recipe           "tacacs", "Installs/Configures tacacs"
