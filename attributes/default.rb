bb6c851bf454ea08a5913cdb086ad239dcf9d41f
alop <alopgeek@gmail.com>
2013-01-18 15:09:38 -0800

    added more attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
 default["tacacs"]["log_dir"] = "/var/log/tacacs"
+default["tacacs"]["default"] = "/etc/default/tacacs+"
+default["tacacs"]["config"] = "/etc/tacacs+/tac_plus.conf"
+default["tacacs"]["init"] = "/etc/init.d/tacacs_plus"
+default["tacacs"]["acct_log_path"] = "#{node["tacacs"]["log_dir"]}/tac_plus.acct"
